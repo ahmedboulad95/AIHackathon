bb1d57f60912369b15d0dff8c67bb6c73b35c67b
ahmed.n.boulad <ahmed.n.boulad@accenture.com>
2018-05-17 19:10:08 -0500

    Added components and services folders

---
+++ src/app/components/graph/graph.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { BaseChartDirective } from 'ng2-charts/ng2-charts';
+import { DataService } from '../../services/data.service';
+
+@Component({
+  selector: 'app-graph',
+  templateUrl: './graph.component.html',
+  styleUrls: ['./graph.component.css']
+})
+export class GraphComponent implements OnInit {
+
+  constructor(private dataService:DataService) { }
+
+  ngOnInit() {
+    console.log('In OnInit');
+    this.dataService.getPosts().subscribe((posts) => {
+      console.log(posts);
+    });
+  }
+
+  // Doughnut
+  public doughnutChartLabels:string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+  public doughnutChartData:number[] = [350, 450, 100];
+  public doughnutChartType:string = 'doughnut';
+
+  // Bar
+  public barChartLabels:string[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartType:string = 'bar';
+  public barChartLegend:boolean = true;
+
+  public barChartData:any[] = [
+    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
+    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+  ];
+
+  // events
+  public chartClicked(e:any):void {
+    console.log(e);
+  }
+
+  public chartHovered(e:any):void {
+    console.log(e);
+  }
+
+}
---
+++ src/app/services/data.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class DataService {
+
+  constructor(public http:Http) { }
+
+  getPosts() {
+    return this.http.get('https://jsonplaceholder.typicode.com/posts')
+      .pipe(map(res => res.json()));
+  }
+}
